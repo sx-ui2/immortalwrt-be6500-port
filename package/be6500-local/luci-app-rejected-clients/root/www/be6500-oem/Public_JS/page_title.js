727fa24a6d522341dbfa6ad55510c36984d8abf4
关鹏 <nara0318@guanpengdeMacBook-Air.local>
2026-09-20 02:01:07 +0800

    cleanup(luci): remove unused OEM web assets

--- package/be6500-local/luci-app-rejected-clients/root/www/be6500-oem/Public_JS/page_title.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  const model = $.cookie("model");
-  if (model === 'ZFXY-CRB') {
-    document.title = '中扶星云CRB'
-  } else {
-    document.title = '京东云无线宝'
-  }
-});
